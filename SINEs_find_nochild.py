f366a17e08d4cf61c2118f814c9f1cc04ee10b81
Annibal <hackingfactory@gmail.com>
2016-01-26 18:43:27 +0100

    No child ver. 2.1
    Added automatic background peak value calculation

--- SINEs_find_nochild.py
+++ SINEs_find_nochild.py
@@ -3,7 +3,7 @@ from Bio.Emboss.Applications import NeedleCommandline
 from Bio import AlignIO
 from pybedtools import BedTool
 
-parser = argparse.ArgumentParser(description='This script takes a coverage file in BAM or BEDGRAPH format and an annotation file of SINEs in GTF format to find genuine SINE transcripts. Version 2.0 January 2016', epilog='Written by Davide Carnevali davide.carnevali@nemo.unipr.it')
+parser = argparse.ArgumentParser(description='This script takes a coverage file in BAM or BEDGRAPH format and an annotation file of SINEs in GTF format to find genuine SINE transcripts. Version 2.1 January 2016', epilog='Written by Davide Carnevali davide.carnevali@nemo.unipr.it')
 parser.add_argument("-s", "--stranded", help="Use this option if using a stranded coverage file(s). If using bam file make sure it is generated with TopHat as this program use the 'XS' tag to identify the strand of the transcript from which the reads come from", action="store_true")
 parser.add_argument("-t", "--filetype", choices=['bam', 'bg'], help="specify coverage file type: default 'bam'.  Bedgraph stranded files should be comma separated, with plus signal preceding the minus one", default='bam')
 parser.add_argument("-p", "--peak", type= int, help="Set the minimum coverage signal for the SINE to be considered. Default: 10", default='10')
@@ -28,6 +28,8 @@ cvg = HTSeq.GenomicArray("auto", stranded=False, typecode="i")
 left = 0
 central = 0
 right = 0
+count_plus = 0
+count_minus = 0
 alu_list = []
 char = re.compile('-*')
 char2 = re.compile('[-ATGC]*')
@@ -35,53 +37,65 @@ char2 = re.compile('[-ATGC]*')
 # Build the coverage vectors for + and - strand based on XS tag, using uniquely mapped reads
 
 def cvg_bam(file):
+    global count_plus
+    global count_minus
     for read in file:
         if read.aligned and read.optional_field('NH') == 1:
             if read.optional_field('XS') == '+':
                 for cigopt in read.cigar:
                     if cigopt.type == 'M':
                         cvg_plus[cigopt.ref_iv] += 1
+                        count_plus += cigoption.length
             elif read.optional_field('XS') == '-':
                 for cigopt in read.cigar:
                     if cigopt.type == 'M':
                         cvg_minus[cigopt.ref_iv] += 1
-    return (cvg_plus, cvg_minus)
+                        count_minus += cigoption.length
+    return (cvg_plus, cvg_minus, count_plus, count_minus)
 
 def cvg_bam_unstranded(file):
+    global count
     for read in file:
         if read.aligned and read.optional_field('NH') == 1:
             for cigopt in read.cigar:
                 if cigopt.type == 'M':
                     cvg[cigopt.ref_iv] += 1
-    return cvg
+                    count += cigoption.length
+    return (cvg, count)
  
 
 # Build the coverage vectors for + and - strand based on bedgraph files   
 
 def cvg_bedgraph(file_plus, file_minus):
+    global count_plus
+    global count_minus
     for line in open (file_plus):
         if 'chr' in line:
             chrom, start, end, value = line.strip().split("\t")
             cvg_plus[HTSeq.GenomicInterval(chrom, int(start), int(end))] = int(float(value))
+            count_plus += int(float(value))
     for line in open (file_minus):
         if 'chr' in line:
             chrom, start, end, value = line.strip().split("\t")
             cvg_minus[HTSeq.GenomicInterval(chrom, int(start), int(end))] = int(float(value))
-    return (cvg_plus, cvg_minus)
+            count_minus += int(float(value))
+    return (cvg_plus, cvg_minus, count_plus, count_minus)
 
 def cvg_bedgraph_unstranded(file):
+    global count
     for line in open (file):
         if 'chr' in line:
             chrom, start, end, value = line.strip().split("\t")
             cvg[HTSeq.GenomicInterval(chrom, int(start), int(end))] = int(float(value))
-    return cvg
+            count += int(float(value))
+    return (cvg, count)
 
 # Calculate coverage in the Left/Center/Right arm of the SINEs
 
-def frf_stranded(gtf):
+def frf_stranded(gtf, peak):
     for element in gtf:
         if element.iv.strand == '+':
-            if sum(list(cvg_plus[HTSeq.GenomicInterval(element.iv.chrom, element.iv.start, element.iv.end)])) > ((element.iv.end - element.iv.start) * args.peak):
+            if sum(list(cvg_plus[HTSeq.GenomicInterval(element.iv.chrom, element.iv.start, element.iv.end)])) > ((element.iv.end - element.iv.start) * peak):
                 if "MIR" in element.attr['gene_id'] or "Alu" in element.attr['gene_id']:
                     aln_start, aln_end = needle(element.iv.chrom, element.iv.start, element.iv.end, element.attr['gene_id'], element.score, element.iv.strand)
                     max_coverage = max(list(cvg_plus[HTSeq.GenomicInterval(element.iv.chrom, element.iv.start, element.iv.end)]))
@@ -99,7 +113,7 @@ def frf_stranded(gtf):
                 continue
                     
         elif element.iv.strand == '-':
-            if sum(list(cvg_minus[HTSeq.GenomicInterval(element.iv.chrom, element.iv.start, element.iv.end)])) > ((element.iv.end - element.iv.start) * args.peak):
+            if sum(list(cvg_minus[HTSeq.GenomicInterval(element.iv.chrom, element.iv.start, element.iv.end)])) > ((element.iv.end - element.iv.start) * peak):
                 if "MIR" in element.attr['gene_id'] or "Alu" in element.attr['gene_id']:
                     aln_start, aln_end = needle(element.iv.chrom, element.iv.start, element.iv.end, element.attr['gene_id'], element.score, element.iv.strand)
                     max_coverage = max(list(cvg_minus[HTSeq.GenomicInterval(element.iv.chrom, element.iv.start, element.iv.end)]))
@@ -116,15 +130,15 @@ def frf_stranded(gtf):
             else:
                 continue
                 
-        if left < (100 * args.peak) and left < (central/aln_end)*50 and right < (central/aln_end)*200 and out < (100*args.peak) and out < (central/aln_end)*50:
-            alu_list.append([aln_start, aln_end, element.attr['transcript_id'], element.iv.chrom, element.iv.start, element.iv.end, element.iv.strand, left, central, right, out, left_max, max_coverage, right_max, out_max])
+        if left < (200 * peak) and left < (central/aln_end)*100 and right < (central/aln_end)*200 and out < (200*args.peak) and out < (central/aln_end)*100:
+            alu_list.append([peak, aln_start, aln_end, element.attr['transcript_id'], element.iv.chrom, element.iv.start, element.iv.end, element.iv.strand, left, central, right, out, left_max, max_coverage, right_max, out_max])
                 
 
     
-def frf_unstranded(gtf):
+def frf_unstranded(gtf,peak):
     for element in gtf:
         if element.iv.strand == '+':
-            if sum(list(cvg[HTSeq.GenomicInterval(element.iv.chrom, element.iv.start, element.iv.end)])) > (element.iv.end - element.iv.start) * args.peak:
+            if sum(list(cvg[HTSeq.GenomicInterval(element.iv.chrom, element.iv.start, element.iv.end)])) > ((element.iv.end - element.iv.start) * peak):
                 if "MIR" in element.attr['gene_id'] or "Alu" in element.attr['gene_id']:
                     aln_start, aln_end = needle(element.iv.chrom, element.iv.start, element.iv.end, element.attr['gene_id'], element.score, element.iv.strand)
                     max_coverage = max(list(cvg[HTSeq.GenomicInterval(element.iv.chrom, element.iv.start, element.iv.end)]))
@@ -142,7 +156,7 @@ def frf_unstranded(gtf):
                 continue
         
         elif element.iv.strand == '-':
-            if sum(list(cvg[HTSeq.GenomicInterval(element.iv.chrom, element.iv.start, element.iv.end)])) > (element.iv.end - element.iv.start) * args.peak:
+            if sum(list(cvg[HTSeq.GenomicInterval(element.iv.chrom, element.iv.start, element.iv.end)])) > ((element.iv.end - element.iv.start) * peak):
                 if "MIR" in element.attr['gene_id'] or "Alu" in element.attr['gene_id']:
                     aln_start, aln_end = needle(element.iv.chrom, element.iv.start, element.iv.end, element.attr['gene_id'], element.score, element.iv.strand)
                     max_coverage = max(list(cvg[HTSeq.GenomicInterval(element.iv.chrom, element.iv.start, element.iv.end)]))
@@ -158,8 +172,8 @@ def frf_unstranded(gtf):
             else:
                 continue
                 
-        if left < (100 * args.peak) and left < (central/aln_end)*50 and right < (central/aln_end)*200 and out < (100*args.peak) and out < (central/aln_end)*50:
-            alu_list.append([aln_start, aln_end, element.attr['transcript_id'], element.iv.chrom, element.iv.start, element.iv.end, element.iv.strand, left, central, right, out, left_max, max_coverage, right_max, out_max])
+        if left < (200 * peak) and left < (central/aln_end)*100 and right < (central/aln_end)*200 and out < (200*args.peak) and out < (central/aln_end)*100:
+            alu_list.append([peak, aln_start, aln_end, element.attr['transcript_id'], element.iv.chrom, element.iv.start, element.iv.end, element.iv.strand, left, central, right, out, left_max, max_coverage, right_max, out_max])
 
 # Perform global alignment, with Needle algorithm, of the element to its consensus sequence to define the start/end of the central region
         
@@ -218,17 +232,19 @@ if args.filetype == 'bam':
     if args.stranded:
         print "Start reading bam file, this will take a while....."
         cvg_bam(bamfile)
+        peak = int(round((count_plus + count_minus)/6000000, 0))
         cvg_plus.write_bedgraph_file( args.output + "_plus.bg" )
         cvg_minus.write_bedgraph_file( args.output + "_minus.bg" )
         print "Start applying Flanking Region Filter"
-        frf_stranded(annotation)
+        frf_stranded(annotation, peak)
 
     else:
         print "Start reading bam file, this will take a while....."
         cvg_bam_unstranded(bamfile)
+        peak = int(round(count/3000000, 0))
         cvg.write_bedgraph_file( args.output + ".bg" )
         print "Start applying Flanking Region Filter"
-        frf_unstranded(annotation)
+        frf_unstranded(annotation, peak)
 
 elif args.filetype == 'bg':
     if args.stranded:
@@ -236,15 +252,17 @@ elif args.filetype == 'bg':
         bedgraph_minus = args.coverage.strip().split(",")[1]
         print "Start reading bedgraph file, this will take a while....."
         cvg_bedgraph(bedgraph_plus, bedgraph_minus)
+        peak = int(round((count_plus + count_minus)/6000000000, 0))
         print "Time elapsed %s" %(time.time() - start_time)
         print "Start applying Flanking Region Filter"
-        frf_stranded(annotation)
+        frf_stranded(annotation, peak)
     else:
         bedgraph = args.coverage
         print "Start reading bedgraph file, this will take a while....."
         cvg_bedgraph_unstranded(bedgraph)
+        peak = int(round(count/3000000, 0))
         print "Start applying Flanking Region Filter"
-        frf_unstranded(annotation)
+        frf_unstranded(annotation, peak)
 
 
 
